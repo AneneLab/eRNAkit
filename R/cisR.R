95371f76b2eef52f88b3a41f2d47c7f7e47e9bd9
Chinedu Anene <caanenedr@outlook.com>
2025-07-26 17:41:51 +0100

    Updated variant scoring logic
    Added extended workflows for variant scoring.

--- R/cisR.R
+++ R/cisR.R
@@ -1,3 +1,71 @@
+#' Identify Potential Regulatory Variants
+#'
+#' This function predicts potential regulatory non-coding variants based on target mRNA decay and translation efficiency (TE).
+#' It is tailored for exploratory use in **single-sample scenarios**, such as variant interpretation, rather than full expression-based
+#' prioritisation across a cohort.
+#'
+#' The input `input` must be a data frame containing only the variants. It must include
+#' **three columns** named **chr**, **start**, and **end**. Start and end must be the same value.
+#'
+#' @param input Data frame containing variants. Must have exactly three columns: `chr`,
+#'   `start`, and `end`.
+#' @param fdr Numeric. FDR threshold for significance (default is 0.1).
+#' @param db eRNAkitDB object.
+#'
+#' @return A named list with result objects.
+#'
+#' @examples
+#' # Assuming input_df has columns chr, start, end
+#' # and eRNAkitDB is loaded with required tables:
+#' result <- to_dete(input_df, fdr = 0.05, db = eRNAkitDB)
+#'
+#' @export
+cisR_explore <- function(input, fdr=0.1, db=eRNAkitDB){
+
+  tab <- input
+  tab$chr <- sub("chr", "", tab$chr)
+  tab$strand <- "*"
+  tab$id <- paste0("row", rownames(tab))
+
+  ## Collect
+  cisRdb <- list()
+  cisRdb[["var"]] <- tab
+
+  ## Intersect
+  tab <- eRNAkit::getGRange(tab)
+  etab <- eRNAkit::getGRange(db$core)
+  hits <- GenomicRanges::findOverlaps(tab, etab)
+
+  # Match
+  matched <- cbind(
+    as.data.frame(S4Vectors::mcols(tab)[S4Vectors::queryHits(hits), ]),
+    as.data.frame(S4Vectors::mcols(etab)[S4Vectors::subjectHits(hits), ])
+  )
+
+  if (nrow(matched) == 0) {
+    message("No variant–eRNA intersect found")
+    return(NULL)
+  }
+
+  matched <- matched[c(1, 7)]
+  names(matched) <- c("id", "E")
+
+
+  ## Add to collection
+  cisRdb[["var2erna"]] <- matched
+  cisRdb[["einteraction"]] <- eRNAkit::link2rr(cisRdb$var2erna, db$R2R)
+
+  dete <- eRNAkit::to_dete(cisRdb$einteraction$E, db, t=fdr)
+  cisRdb[["e2decay"]] <- dete$de
+  cisRdb[["e2TE"]] <- dete$te
+
+
+  return(cisRdb)
+}
+
+
+
+
 #' Prioritize Functional Regulatory Variants Using eRNA–mRNA Interactions.
 #'
 #' This function identifies and prioritizes putative functional regulatory non-coding variants
@@ -27,9 +95,115 @@ cisR <- function(vcf="path/to/vcf", E="eRNA", G="mRNA",
   cisRdb[["var"]] <- eRNAkit::process_vcf(vcf, n=n)
   cisRdb[["var2erna"]] <- eRNAkit::var2erna(cisRdb$var, db$core)
   cisRdb[["einteraction"]] <- eRNAkit::link2rr(cisRdb$var2erna, db$R2R)
-  cisRdb[["varReg"]] <- eRNAkit::varReg(cisRdb$var, cisRdb$var2erna,
-                               cisRdb$einteraction, E=E, G=G, t=fdr)
-  return(cisRdb)
+
+  dete <- eRNAkit::to_dete(cisRdb$einteraction$E, db, t=fdr)
+  cisRdb[["e2decay"]] <- dete$de
+  cisRdb[["e2TE"]] <- dete$te
+
+  var <- eRNAkit::varReg(cisRdb$var, cisRdb$var2erna,
+                         cisRdb$einteraction, E=E, G=G, t=fdr)
+  cisRdb$E <- var$E
+  cisRdb$G <- var$G
+
+  result <- eRNAkit::cisR_Score(cisRdb)
+  result$RAW <- cisRdb
+
+
+  return(result)
+}
+
+
+#' Score Evidence of Regulatory Variants
+#'
+#' This function assigns quantitative scores to variants in a the cisRdb interaction database
+#' based on multiple layers of evidence, including eRNA impact,eRNA-gene regulation,
+#' decay impact, and translation. It outputs a final classification of each variant into
+#' one of three evidence classes: "empirical", "predictive", or "weak".
+#'
+#' @param cisRdb A list containing regulatory interaction data from cisR run, with the following named components:
+#' \describe{
+#'   \item{einteraction}{Data frame of enhancer-variant-gene interactions with columns \code{id}, \code{E}, and \code{G}.}
+#'   \item{var2erna}{Data frame mapping variant IDs to enhancer IDs with columns \code{id} and \code{E}.}
+#'   \item{E}{Data frame of enhancer-only analyses, including a \code{sig} column and a \code{trend} column.}
+#'   \item{G}{Data frame of enhancer-to-gene analysis, with a \code{sig} column and a \code{trend} column.}
+#'   \item{e2TE}{Data frame with translation and decay evidence, including \code{pair}, \code{sig}, and \code{trend} columns.}
+#' }
+#'
+#' @return A list with two elements:
+#' \describe{
+#'   \item{\code{variants}}{A data frame with unique variant identifiers and the highest evidence score observed across interactions. Contains columns \code{chr}, \code{start}, \code{ref}, \code{alt}, \code{id}, \code{score}, and \code{s.class} (evidence class).}
+#'   \item{\code{evidence}}{The full interaction-level data frame annotated with component evidence and intermediate scores.}
+#' }
+#'
+#' @details
+#' The scoring is additive, with each layer contributing as follows:
+#' \itemize{
+#'   \item Enhancer-only (E.impact): 5 points
+#'   \item Enhancer-gene (E2G.impact): 2.5 points
+#'   \item Decay impact: 1 point
+#'   \item Translation impact: 1 point
+#' }
+#' All variants start with a base score of 0.5. Final classifications are assigned based on the score:
+#' \itemize{
+#'   \item \code{empirical}: score ≥ 6.5
+#'   \item \code{predictive}: 1.5 ≤ score < 6.5
+#'   \item \code{weak}: score < 1.5
+#' }
+#'
+#' @examples
+#' # Assuming cisRdb is a properly formatted list as described above
+#' result <- cisR_Score(cisRdb)
+#' head(result$variants)
+#' head(result$evidence)
+#'
+#' @export
+cisR_Score <- function(cisRdb){
+  # Extract impact
+  extract_cisRdb <- function(x, id, name) {
+    x <- x[x$sig %in% "sig", ]
+    x <- x[, c(id, "trend")]
+    names(x) <- c(id, name)
+    return(x)
+  }
+
+  base <- merge(cisRdb$einteraction, cisRdb$var2erna, by="E")
+  base$varE <- paste0(base$id, "_", base$E)
+
+  empE <- extract_cisRdb(cisRdb$E, "varE", "E.impact")
+  emp <- extract_cisRdb(cisRdb$G, "varEG", "E2G.impact")
+  emp1 <- extract_cisRdb(cisRdb$e2TE, "pair", "TE.impact")
+  emp2 <- extract_cisRdb(cisRdb$e2TE, "pair", "decay.impact")
+
+  ## Merge
+  base <- merge(base, empE, by="varE", all=T)
+  base$varEG <- paste0(base$varE, "_", base$G)
+  base <- merge(base, emp, by="varEG", all=T)
+  dete_impact <- merge(emp2, emp1, by="pair", all=T)
+  base <- merge(base, dete_impact, by="pair", all=T)
+
+  #
+  sp_id <- as.data.frame(do.call(rbind,
+                                 strsplit(base$id, split = "[:_]",
+                                          fixed = FALSE)))
+  colnames(sp_id) <- c("chr", "start", "ref", "alt")
+  sp_id$id <- base$id
+  base <- base[, !names(base) %in% c("pair", "varEG", "varE", "id")]
+  base <- cbind(sp_id, base)
+
+  base$score <- 0.5
+  base$score <- base$score +
+    ifelse(!is.na(base$E.impact), 5, 0) +
+    ifelse(!is.na(base$E2G.impact), 2.5, 0) +
+    ifelse(!is.na(base$decay.impact),        1, 0) +
+    ifelse(!is.na(base$TE.impact),     1, 0)
+
+
+  ###
+  res <- aggregate(score ~ chr + start + ref + alt + id, data = base, FUN = max)
+  res$s.class <- ifelse(res$score >= 6.5, "empirical",
+                        ifelse(res$score >= 1.5, "predictive", "weak") )
+
+  return(list("variants"=res, "evidence"=base))
 }
 
 
@@ -57,7 +231,7 @@ cisR <- function(vcf="path/to/vcf", E="eRNA", G="mRNA",
 #'   columns = samples). Row names should be eRNAkit IDs "enxxx".
 #' @param G Numeric matrix or \code{data.frame} of mRNA expression (rows = mRNAs,
 #'   columns = samples). Row names should be Ensemble IDs to match output1-3.
-#'   @param t False discovery rate threshold for filtering results (default: `0.1`).
+#'   @param t False discovery rate threshold for filtering G results (default: `0.1`).
 #'
 #' @return A named \code{list} with components:
 #' \describe{
@@ -112,6 +286,16 @@ varReg <- function(output, output2,
   E.prior <- eRNAkit::ebayes_prior(log_E)
   G.prior <- eRNAkit::ebayes_prior(log_G)
 
+  # Helper
+  pp_out <- function(df, is, fdrt = 0.05) {
+    df <- do.call(rbind, df)
+    df$sig <- ifelse(round(df$local.FDR, 3) <= fdrt, "sig", "")
+    df$trend <- ifelse(df$logFC > 0, "Increasing", "Decreasing")
+    df$is <- is
+    rownames(df) <- NULL
+    return(df)
+  }
+
   ## Analyse E
   bout <- lapply(output2$id, function(v){
     varSamples <- output[output$id %in% v, ][["sample"]]
@@ -135,19 +319,17 @@ varReg <- function(output, output2,
 
     fit$var <- v
     fit$ID <- rownames(e_exp)
+    fit$n.var <- length(varSamples)
 
     return(fit)
   })
-  bout <- do.call(rbind, bout)
-  bout$sig <- ifelse(bout$local.FDR <= t, "sig", "")
-  bout$is <- "E"
-  rownames(bout) <- NULL
-
-  # Restrict
-  output2b <- bout[bout$sig %in% "sig", ][["var"]]
+  ## The FDR here is the same as p.value due to single E tested
+  bout <- pp_out(bout, "E", 0.05)
+  bout$varE <- paste0(bout$var, "_", bout$ID)
+  sigvar <- bout[bout$sig %in% "sig", ][["var"]]
 
   ## Analyse linked G
-  bout2 <- lapply(output2b, function(v){
+  bout2 <- lapply(sigvar, function(v){
     varSamples <- output[output$id %in% v, ][["sample"]]
     er <- output2[output2$id %in% v, ][["E"]]
     eInter <- output3[output3$E %in% er, ]
@@ -171,31 +353,79 @@ varReg <- function(output, output2,
 
     fit$var <- v
     fit$ID <- rownames(e_exp)
+    # This is valid because 1:1 E to var mapping
+    fit$E <- er
+    fit$n.var <- length(varSamples)
 
     return(fit)
   })
-  bout2 <- do.call(rbind, bout2)
-  bout2$sig <- ifelse(bout2$local.FDR <= t, "sig", "")
-  bout2$is <- "G"
-  rownames(bout2) <- NULL
+  bout2 <- pp_out(bout2, "G", 0.1)
+  bout2$varEG <- paste0(bout2$var, "_", bout2$E, "_", bout2$ID)
 
-  # NOTE: The use of FDR 0.1 at both level is justified.
-  # Final FDR will be 0.1 * 0.1 = 1%
-  ghit <- bout2[bout2$sig %in% "sig", ]
-  ehit <- bout[bout$var %in% ghit$var, ]
-  message(paste("Identifed ", nrow(ehit), "E hits", " with",
-                nrow(ghit), " associated G."))
-
-  hits <- rbind(ehit, ghit)
-  hits$dir.var <- ifelse(hits$logFC > 0, "up", "down")
-  hits <- hits[order(hits$var), ]
-
-  if (nrow(hits) == 0) {
-    message("No significant hits.")
+  return(list("E"=bout, "G"=bout2))
   }
 
-  return(list("hits"=hits, "E"=bout, "G"=bout2))
+
+#' Link a given eRNA to significant impact on mRNA decay or translation efficiency
+#'
+#' This function processes the "eDecay2mRNA" and "TE2mRNA" slots on eRNAkitDB to get their impact on target mRNA.
+#' It requires the "G" object in the DB map the gene symbol of those slots to the Ensemble id.
+#'
+#' @param E A vector containing eRNAs of interest. eRNA IDs must match the naming in eRNAkitDB.
+#' @param DB The eRNAkitDB object.
+#' @param t Numeric threshold for significance on adjusted p-values (False Discovery Rate). Default is 0.1.
+#'
+#' @return A named list with two elements:
+#' \itemize{
+#'   \item \code{de}: data.frame of significant mRNA decay impact.
+#'   \item \code{te}: data.frame of significant translational efficiency impact.
+#' }
+#'
+#' @details
+#' This expects the exact slot naming convention from the eRNAkitDB.
+#' The output retains the columns in the eRNAkitDB slots but replaces pair for easy mapping.
+#'
+#' @examples
+#' \dontrun{
+#' results <- to_dete(E = eRNAs,
+#'                   DB = eRNAkitDB,
+#'                   t = 0.1)
+#' head(results$de)
+#' head(results$te)
+#' }
+#'
+#' @export
+to_dete <- function(E, DB, t=0.1){
+  de <- DB$eDecay2mRNA[DB$eDecay2mRNA$E %in% E, ]
+  te <- DB$TE2mRNA[DB$TE2mRNA$E %in% E, ]
+
+  # Helper
+  processTAB <- function(df) {
+    # FDR per E
+    out <- lapply(split(df, df$E), function(subdf) {
+      subdf$local.FDR <- p.adjust(subdf$pvalue, method = "BH")
+      return(subdf)
+    })
+    out <- do.call(rbind, out)
+    rownames(df) <- NULL
+
+    # Get significant
+    out$sig <- ifelse(out$local.FDR <= t, "sig", "")
+    out <- out[out$sig %in% "sig", ]
+
+    out <- merge(DB$G[c("ID", "symbol")], out,
+                 by.x = "symbol", by.y = "G", all.y = TRUE)
+
+    out$pair <- paste0(out$E, "_", out$ID)
+
+    return(out)
   }
+
+  de <- processTAB(de)
+  te <- processTAB(te)
+
+ return(list("de"=de, "te"=te))
+}
 
 
 #' Link eRNAs to their Regulatory Targets
